41e67aff8a793157ec5fbf49dd2d06cb78a6f578
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2023-03-09 17:34:07 +0100

    Starting with login page

--- humans-vs-zombies-FE/src/app/app-routing.module.ts
+++ humans-vs-zombies-FE/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPage } from './pages/login/login.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", 
+    component: LoginPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
