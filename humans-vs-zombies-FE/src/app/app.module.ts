b0020c46c8910177faf27cdb3dd5616baf315c20
Abdo Taleb <44838067+AbdoTaleb@users.noreply.github.com>
2023-03-10 00:19:21 +0100

    Starting with login form

--- humans-vs-zombies-FE/src/app/app.module.ts
+++ humans-vs-zombies-FE/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPage
+    LoginPage,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
