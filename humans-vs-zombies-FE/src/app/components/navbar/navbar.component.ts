e660b0419abfa120a1f42f6dc47c17cd1da9cafb
abdo <abdo.taleb1@hotmail.com>
2023-03-15 13:14:31 +0100

    Starting with creating game

--- humans-vs-zombies-FE/src/app/components/navbar/navbar.component.ts
+++ humans-vs-zombies-FE/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { Game } from 'src/app/models/game.model';
+import { environment } from 'src/environments/environment';
+const {APIGames} = environment;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,14 +10,25 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
+  //  private _newGame: Game[] = []
   showModal = false;
   showCreateGameModal = false;
-    toggleModal() {
-      this.showModal = !this.showModal;
-    }
-    toggleCreateGameModal() {
-      this.showCreateGameModal = !this.showCreateGameModal;
-    }
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+  toggleCreateGameModal() {
+    this.showCreateGameModal = !this.showCreateGameModal;
+  }
+
+  constructor(private readonly http: HttpClient) {}
+
+  onGameCreate (game: {gameName: String, gameStartTime: any, gameEndTime: any, nwLat: String, nwLng: string, seLat: String, seLng: String} ){
+    console.log(game);
+    this.http.post(APIGames, JSON.stringify(game))
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
     
 
 }
