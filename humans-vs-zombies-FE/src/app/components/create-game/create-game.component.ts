3dd432b72f582b12121722c1055a8723ee76bfc1
Melvin <m.hedenfalk@gmail.com>
2023-03-28 15:19:55 +0200

    Removed redundant code in create game

--- humans-vs-zombies-FE/src/app/components/create-game/create-game.component.ts
+++ humans-vs-zombies-FE/src/app/components/create-game/create-game.component.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Location } from '@angular/common';
-import { Router } from '@angular/router';
 
 const { APIGames } = environment;
 
@@ -15,21 +13,7 @@ export class CreateGameComponent {
 
   constructor(
     private readonly http: HttpClient,
-    private readonly location: Location,
-    private readonly router: Router,
     ) {}
-
-  private _newGame: any = {
-    "name": "",
-    "startTime": "",
-    "endTime": "",
-    "nwLat": 0,
-    "nwLng": 0,
-    "seLat": 0,
-    "seLng": 0,
-    "maxPlayers": 0,
-    "registeredPlayers": 0
-  };
 
   acceptedTime: boolean = true;
   createdGame: boolean = false;
@@ -41,26 +25,15 @@ export class CreateGameComponent {
 
     onGameCreate (game: {name: String, startTime: String, endTime: String, nwLat: String, nwLng: string, seLat: String, seLng: String, maxPlayers: number, registeredPlayers: number} ){
       game.registeredPlayers = 0;
-      this._newGame = game;
-    console.log(game.maxPlayers);
 
     if (game.endTime > game.startTime){
       this.createdGame = true;
       this.http.post(APIGames + "/add-new-game", game)
-      .subscribe((res) => {
-        console.log(res);
+      .subscribe(() => {
+        window.location.reload();
       });
-      this.location.go(this.location.path());
-      window.location.reload();
-      
     } else {
       this.acceptedTime = false;
     }
-  }
-
-    submit(){
-    console.log("from submit");
-    this.location.replaceState(this.location.path());
-    this.router.navigateByUrl("/game-list-view");
   }
 }
