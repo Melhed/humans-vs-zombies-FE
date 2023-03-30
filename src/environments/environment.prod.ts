15d05975f0a36ba8475a0d71d7a000add90d39cf
Melhed <82815800+Melhed@users.noreply.github.com>
2023-03-30 17:19:16 +0200

    Corrected production env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment = {
-    production: true,
-    //TODO: Add deployed links whne project has been deployed
-    APIUsers: "http://localhost:8080/api/v1/user",
-    APIGames: "http://localhost:8080/api/v1/game",
-    APIKey: "",
-}
+  production: true,
+  APIUsers: 'https://hvz-be.herokuapp.com/api/v1/user',
+  APIGames: 'https://hvz-be.herokuapp.com/api/v1/game',
+  APIMission: 'https://hvz-be.herokuapp.com/api/v1/game/{gameId}/mission',
+  APISquadCheckin:
+    'https://hvz-be.herokuapp.com/api/v1/game/{gameId}/squad/{squadId}/check-in',
+  APIKill: 'https://hvz-be.herokuapp.com/api/v1/game/{gameId}/kill',
+  APIPlayer: 'https://hvz-be.herokuapp.com/api/v1/game/{gameId}/player',
+  APIKey: 'https://hvz-be.herokuapp.com/api/v1/',
+};
