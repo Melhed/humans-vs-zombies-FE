1b374f8b6b9f296ef1bcbdbda6d2c67adf7e3e3b
Melhed <82815800+Melhed@users.noreply.github.com>
2023-03-30 17:39:05 +0200

    Revert "Fixed node version"
    This reverts commit 47698451fff608cfddb4907f4107aeebb47ba27a.

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
 const express = require("express");
 const app = express();
 app.use(requireHTTPS);
@@ -8,11 +15,3 @@ app.get("/*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
-
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
-    return res.redirect("https://" + req.get("host") + req.url);
-  }
-  next();
-}
