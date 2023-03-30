47698451fff608cfddb4907f4107aeebb47ba27a
Melhed <82815800+Melhed@users.noreply.github.com>
2023-03-30 17:34:24 +0200

    Fixed node version

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
-    return res.redirect("https://" + req.get("host") + req.url);
-  }
-  next();
-}
 const express = require("express");
 const app = express();
 app.use(requireHTTPS);
@@ -15,3 +8,11 @@ app.get("/*", function (req, res) {
 });
 
 app.listen(process.env.PORT || 8080);
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
